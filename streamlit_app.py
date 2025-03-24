63612e7cc0aca5e7fce9fc78b14307c152427363
C0DEFROI <frlnmanzo4@gmail.com>
2025-03-24 11:26:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,19 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-helpful_links = [
-    "https://docs.streamlit.io",
-    "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
-    "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
-    "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
-]
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie!:cup_with_straw:")
-st.write("Choose the fruits you want in your custom Smoothie!")
-
-cnx = st.connection("snowflake")
-session = cnx.session()
-
-# st.markdown("""
-# - :page_with_curl: [Streamlit open source documentation](https://docs.streamlit.io)
-# - :snowflake: [Streamlit in Snowflake documentation](https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit)
-# - :books: [Demo repo with templates](https://github.com/Snowflake-Labs/snowflake-demo-streamlit)
-# - :memo: [Streamlit in Snowflake release notes](https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake)
-# """)
-
-# import streamlit as st
-
-# option = st.selectbox(
-#     "What is your Favorite fruit?",
-#     ("Banana", "Strawberries", "Peaches"),
-# )
-
-# st.write("Your Favorite fruit is:", option)
-
-
-#import streamlit as st
+st.write(
+    """Choose the fruits you want in your custom Smoothie!
+    """)
 
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
